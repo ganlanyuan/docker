b816f94b94a32159fed6893f61e53282e2f8af23
William Lin 22 <ganlanyuan@gmail.com>
2016-09-06 15:27:13 -0400

    initial commit

---
+++ web/docker-start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Change present working directory
+# alias myproject="cd web"
+# myproject
+
+# Show present working directory
+pwd
+
+# Run bower install
+bower install
+
+# Run gulp
+gulp
