2869e40db6fa5dfa0bec0a1e0206fc4ed82e2bcb
William Lin <williamlin@Williams-iMac.local>
2018-03-01 21:54:38 -0500

    add puppeteer

--- lighthouse/docker_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Switch if you want to Lighthouse with full Chrome instead of headless.
-# docker build -f Dockerfile.nonheadless -t lighthouse_ci . --build-arg CACHEBUST=$(date +%d)
-
-docker build -t cmcdev/lighthouse:1.0.0 . --build-arg CACHEBUST=$(date +%d)
